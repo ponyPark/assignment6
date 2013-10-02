40fc72a8f05614e61bf98eb0810febe14787caea
jlomeo <JLomeo@JJLMacMini.local>
2013-10-02 18:45:16 -0500

    changes

---
+++ create.php
@@ -0,0 +1,7 @@
+<? 
+
+include 'phpapi.php';
+$phpInit = new phpapi;
+
+
+?>
--- phpapi.php
+++ phpapi.php
@@ -1,8 +1,9 @@
-<? php
+<?
 class phpapi{
 
 	function addUser(){
 		//this will be called when a user is signing up
+		echo('TEST');
 	}
 	function verifyUser(){
 		//this will be called when a user attempts to log into the system
