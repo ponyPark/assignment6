3af3217e507f799b213bb65ed3ecb1d6bebdc969
jlomeo <JLomeo@JJLMacMini.local>
2013-10-02 18:25:48 -0500

    added functions

---
+++ phpapi.php
@@ -0,0 +1,15 @@
+<? php
+class phpapi{
+
+	function addUser(){
+		//this will be called when a user is signing up
+	}
+	function verifyUser(){
+		//this will be called when a user attempts to log into the system
+
+	}
+
+
+
+
+}
