c5e3e0f1546398a89ea42fdd8b165d0be7acb2ce
jlomeo <JLomeo@JJLMacMini.local>
2013-10-02 20:21:57 -0500

    working adduser

--- phpapi.php
+++ phpapi.php
@@ -4,11 +4,42 @@
 class phpapi {
 
     function phpapi() {
-        echo('made');
+	//constructor
     }
 
-	public function addUser(){
-		echo('addUser');	
+	public function addUser(){	
+		//add a user to the system
+		$con = mysql_connect("localhost", "cupcake", "cupcake");
+		if(!$con){
+			die('Could not connect: ' . mysql_error());
+		}
+		mysql_select_db("CustomCupcakes", $con)
+		or die("Unable to select database: " . mysql_error());
+		$mailList = $_POST['mailList'];
+		$fname = $_POST['fname'];
+		$lname = $_POST['lname'];
+		$email = $_POST['email'];
+		$pw = $_POST['pw'];
+		$phone = $_POST['phone'];
+		$address = $_POST['address'];
+		$city = $_POST['city'];
+		$state = $_POST['state'];
+		$zip = $_POST['zip'];
+		$auth = 0;
+		$query = "INSERT INTO Users(MailingList,FirstName,LastName,Email,Password,PhoneNumber,Address,City,State,ZipCode,Privilege) VALUES ('$mailList','$fname','$lname','$email','$pw','$phone','$address','$city','$state','$zip','$auth')";
+
+		
+
+		if(!mysql_query($query,$con)){
+			die('ERROR ' . mysql_error());
+		}
+		else{
+		session_start();
+		$_SESSION['logged'] = true;
+		$_SESSION['userEmail'] = $email;
+		mysql_close($con);
+		header ('Location: order.php');	
+		}
 	
 	}
 
