d192333d8152cc62ad3dd6a51c2b26eb19fba73c
jlomeo <JLomeo@JJLMacMini.local>
2013-10-05 12:28:47 -0500

    Updated CSS File For Justin's Part

--- order.php
+++ order.php
@@ -6,8 +6,7 @@
     session_start();
     //these are the only two session variables that 
     //will be there if the user has successfully loggin into the system
-    echo($_SESSION['userEmail']);
-    echo($_SESSION['logged']);
+
 
 
     ?>
