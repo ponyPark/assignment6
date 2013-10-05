bd586ed0174ababb02b8b5accf90ae4cb7318d33
jlomeo <JLomeo@JJLMacMini.local>
2013-10-05 12:25:43 -0500

    Justin's Part

---
+++ js/script.js
@@ -0,0 +1,133 @@
+/*BAM Software CSS for CustomCupcakes
+October, 2013*/
+function init() {
+    var flavorList = document.getElementById("flavor");
+    var favList = document.getElementById("favUl");
+    var fillingList = document.getElementById("filling");
+    var icingList = document.getElementById("icing");
+    var flavListSelection = flavorList.getElementsByTagName("img");
+    var favListSelection = favList.getElementsByTagName("img");
+    var fillingListSelection = fillingList.getElementsByTagName("img");
+    var icingListSelection = icingList.getElementsByTagName("img");
+    //Test data to be inserted in
+    generateList(flavorList);
+    generateList(fillingList);
+    generateList(icingList);
+
+    function generateList(input) {
+        for (var i = 0; i < 10; i++) {
+            //create new li element
+            var newNumberListItem = document.createElement("li");
+            //create new text node
+            var numberListValue = document.createElement("img");
+            numberListValue.src = 'artwork/banana.PNG';
+            numberListValue.id = "img" + i;
+            var p = document.createElement('p'),
+            // creates a new text-node:
+            text = document.createTextNode('vanilla');
+            // appends the text-node to the newly-created p element:
+            p.appendChild(text);
+            numberListValue.addEventListener("click", selectImage, false);
+            newNumberListItem.appendChild(numberListValue);
+            newNumberListItem.appendChild(p);
+           input.appendChild(newNumberListItem);
+        }
+    }
+
+
+    /*
+    The selectImage function will allow the user to know that they made a selection.
+    */
+    function selectImage(e) {
+
+        removeClasses(e);
+
+    }
+    function removeClasses(e) {
+
+        if (e.target.parentNode.parentNode.id === "filling") {
+            var counter = 0;
+
+            for (var i = 0; i < fillingListSelection.length; i++) {
+                fillingListSelection[i].className = "";
+            }
+            e.target.className = "selected";
+        }
+        if (e.target.parentNode.parentNode.id === "flavor") {
+            var counter = 0;
+            for (var i = 0; i < flavListSelection.length; i++) {              
+                    flavListSelection[i].className = "";
+            }
+            
+            e.target.className = "selected";
+
+        }
+        
+        if (e.target.parentNode.parentNode.id === "favUl") {
+            var counter = 0;
+            for (var i = 0; i < favListSelection.length; i++) {
+                favListSelection[i].className = "";
+            }
+            e.target.className = "selected";
+        }
+        if (e.target.parentNode.parentNode.id === "icing") {
+            var counter = 0;
+            for (var i = 0; i < icingListSelection.length; i++) {         
+                    icingListSelection[i].className = "";
+                }            
+            e.target.className = "selected";
+        }
+    }
+    var overlay = document.getElementById("addFavorite");
+    overlay.addEventListener("click", function () {
+       
+            el = document.getElementById("overlay");
+            el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+    });
+    var exitOverlay = document.getElementById("exit");
+    exitOverlay.addEventListener("click", function () {
+        el = document.getElementById("overlay");
+        el.style.visibility = "hidden";
+    });
+    var saveOverlay = document.getElementById("saveFavorite");
+    saveOverlay.addEventListener("click", function () {
+        var cupcakeName = document.getElementById("cupcakeName");
+        //create new li element
+        var newCupcakeListItem = document.createElement("li");
+        //create new text node
+        var cupcakeListValue = document.createElement("img");
+        cupcakeListValue.src = 'artwork/banana.PNG';
+        var p = document.createElement('alt'),
+        // creates a new text-node:
+        text = document.createTextNode(cupcakeName.value);
+        // appends the text-node to the newly-created p element:
+        p.appendChild(text);
+        cupcakeListValue.addEventListener("click", selectImage, false);
+        newCupcakeListItem.appendChild(cupcakeListValue);
+        newCupcakeListItem.appendChild(p);
+        favList.appendChild(newCupcakeListItem);
+        //close the dialog
+        el = document.getElementById("overlay");
+                el.style.visibility = "hidden";
+    });
+    var resetToppings = document.getElementById("clearAll");
+    resetToppings.addEventListener("click", function () {
+        var fieldSet = document.getElementById('quad').getElementsByTagName("input");
+        var len = fieldSet.length;
+
+        for (i = 0; i < len; i++) {
+            if (fieldSet[i].type === "checkbox") {
+                fieldSet[i].checked = false;
+            }
+        }
+    });
+    var resetCupcake = document.getElementById("resetCupcake");
+    resetCupcake.addEventListener("click", function () {  
+        //TODO
+    });
+    var order = document.getElementById("order");
+    order.addEventListener("click", function () {
+       //TODO
+    });
+}
+window.onload = init;
--- order.php
+++ order.php
@@ -1,10 +1,103 @@
-ORDER.PHP
-<? 
-	session_start();
-	//these are the only two session variables that 
-	//will be there if the user has successfully loggin into the system
-	echo($_SESSION['userEmail']);
-	echo($_SESSION['logged']);
+<!--Ordering Page for CustomCupcakes by BAM Software-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <? 
+    session_start();
+    //these are the only two session variables that 
+    //will be there if the user has successfully loggin into the system
+    echo($_SESSION['userEmail']);
+    echo($_SESSION['logged']);
 
 
-	?>
+    ?>
+    <title>CustomCupcakes Homepage</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Cantarell' rel='stylesheet' type='text/css'>
+    <script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+    <fieldset class="favList">
+        <legend>Favorites List</legend>
+        <ul id="favUl">
+        </ul>
+    </fieldset>
+    <div id="orderingPane">
+        <button id="order" type="button">Order</button>
+    </div>
+    <div id="selectContainer">
+        <h4>Cupcake Flavor</h4>
+        <div class="selectorBox">
+         <!--The ul element will be injected with list elements from javascript-->
+            <ul id="flavor">
+            </ul>
+        </div>
+        <h4>Filling</h4>
+        <!--The ul element will be injected with list elements from javascript-->
+        <div class="selectorBox">
+            <ul id="filling">
+            </ul>
+        </div>
+        <h4>Icing</h4>
+        <!--The ul element will be injected with list elements from javascript-->
+        <div class="selectorBox">
+            <ul id="icing">
+            </ul>
+        </div>
+        <fieldset class="toppingGroup">
+            <legend>Toppings</legend>
+            <ul id="quad">
+                <li>
+                    <input type="checkbox" id="t1" class="topping" value="sprinkles" /><label for="t1">Sprinkles</label></li>
+                <li>
+                    <input type="checkbox" id="t2" class="topping" value="bacon" /><label for="t2">Bacon</label></li>
+                <li>
+                    <input type="checkbox" id="t3" class="topping" value="m&ms" /><label for="t3">M&Ms</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="t4" class="topping" value="reese's pieces" /><label for="t4">Reese's Pieces</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="t5" class="topping" value="skittles" /><label for="t5">Skittles</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="t6" class="topping" value="mini chocolate chips" /><label for="t6">Mini Chocolate Chips</label></li>
+                <li>
+                    <input type="checkbox" id="t7" class="topping" value="oreo bits" /><label for="t7">Oreo bits</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="t8" class="topping" value="twix bits" /><label for="t8">Twix bits</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="t9" class="topping" value="butterfinger bits" /><label for="t9">Butterfinger bits</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="t10" class="topping" value="snicker bits" /><label for="t10">Snicker bits</label></li>
+                <li>
+                    <input type="checkbox" id="t11" class="topping" value="mini marshmellows" /><label for="t11">Mini marshmellows</label></li>
+            </ul>
+            <button id="clearAll" type="button">Clear all toppings</button>
+        </fieldset>
+    </div>
+    <footer>
+        <button id="resetCupcake" type="button">Reset Cupcake</button>
+        <button id="updateOrder" type="button">Update Order</button>
+        <input id="quan" type="number" value="1" name="quantity" min="1" max="50">  
+        <button id="addFavorite" type="button">Add to Favorites</button>
+    </footer>
+    <div id="overlay">
+     <div>
+          <p>Cupcake name: <input type="text" id="cupcakeName" ></p>
+           <ul>
+               <li id="cupcakeFlavor">Cupcake Flavor:</li>
+               <li id="cupcakeFilling">Fillings:</li>
+               <li id="cupcakeIcing">Icing:</li>
+               <li id="cupcakeTopping">Toppings:</li>
+           </ul>
+          <button id="saveFavorite" type="button">Save</button>
+          <button id="exit" type="button">Exit</button>
+     </div>
+            </div>        
+</body>
+</html>
