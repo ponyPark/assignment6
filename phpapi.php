501fe3dfc263934d423823e7539e372dc1b070df
JKayse <JKayse@smu.edu>
2013-10-05 18:33:20 -0500

    Added the insert favorites function, and moved the connection for the database to the constructor so we don't need to keep adding it every function.

--- phpapi.php
+++ phpapi.php
@@ -3,18 +3,21 @@
 
 class phpapi {
 
-    function phpapi() {
-	//constructor
+    function phpapi()
+    {
+	    //constructor,
+	    $con = mysql_connect("localhost", "cupcake", "cupcake");
+	    if(!$con)
+	    {
+		    die('Could not connect: ' . mysql_error());
+	    }
+	    mysql_select_db("CustomCupcakes", $con)
+	    or die("Unable to select database: " . mysql_error());
     }
 
-	public function addUser(){	
+	public function addUser()
+    {	
 		//add a user to the system
-		$con = mysql_connect("localhost", "cupcake", "cupcake");
-		if(!$con){
-			die('Could not connect: ' . mysql_error());
-		}
-		mysql_select_db("CustomCupcakes", $con)
-		or die("Unable to select database: " . mysql_error());
 		$mailList = $_POST['mailingList'];
 		$fname = $_POST['fname'];
 		$lname = $_POST['lname'];
@@ -27,58 +30,49 @@ class phpapi {
 		$zip = $_POST['zip'];
 		$auth = 0;
 		$query = "INSERT INTO Users(MailingList,FirstName,LastName,Email,Password,PhoneNumber,Address,City,State,ZipCode,Privilege) VALUES ('$mailList','$fname','$lname','$email','$pw','$phone','$address','$city','$state','$zip','$auth')";
-
-		
-
-		if(!mysql_query($query,$con)){
-			die('ERROR ' . mysql_error());
+		if(!mysql_query($query))
+        {
+			return false;
 		}
-		else{
+		else
+        {
 			//START SESSION
-		session_start();
-		$_SESSION['logged'] = true; //a user will be logged in if this variable is true
-		$_SESSION['userEmail'] = $email; //passing on the user's email for next page to use
-		mysql_close($con);
-		header ('Location: order.php');	
+		    session_start();
+		    $_SESSION['logged'] = true; //a user will be logged in if this variable is true
+		    $_SESSION['userEmail'] = $email; //passing on the user's email for next page to use
+		    header ('Location: order.php');	
 		}
 	
 	}
 
-	public function verifyUser(){
+	public function verifyUser()
+    {
 		//verify a user and start a new session
-		$con = mysql_connect("localhost", "cupcake", "cupcake");
-		if(!$con){
-			die('Could not connect: ' . mysql_error());
-		}
-		mysql_select_db("CustomCupcakes", $con)
-		or die("Unable to select database: " . mysql_error());
 		$pw = hash(md5, $_POST['pw']);
 		$query = "select * from Users where Email = '";
 		$query = $query . $_POST['email'] . "' and Password = '" . $pw ."'";
-
 		$result = mysql_query($query);
 		$info = mysql_fetch_array( $result );
-		
-
-		if(!mysql_query($query,$con)){
-			die('ERROR ' . mysql_error());
+		if(!mysql_query($query))
+        {
+			return false;
 		}
 		if(mysql_num_rows($result) == 0)
 			header ('Location: error.html');
 		$ipaddress = substr((string)$_SERVER['REMOTE_ADDR'], 0,7);
 		
-		if($info['Privilege'] == 1 && $ipaddress == "129.119"){
-			mysql_close($con);
-			header ('Location: analytics.php');	}
-		if(mysql_num_rows($result) > 0){
+		if($info['Privilege'] == 1 && $ipaddress == "129.119")
+        {
+			header ('Location: analytics.php');
+        }
+		if(mysql_num_rows($result) > 0)
+        {
 			session_start();
 			$_SESSION['logged'] = true;
 			$_SESSION['userEmail'] = $info['Email'];
 			//Added the user to the session since we use
 			//that for adding favorites, etc.
 			$_SESSION['user'] = $info['UserID'];
-			mysql_close($con);
-			echo($info['Privilege']);
 			header ('Location: order.php');	
 		}
 
@@ -87,20 +81,38 @@ class phpapi {
 	
 	public function addFavorites()
 	{
-		$con = mysql_connect("localhost", "cupcake", "cupcake");
-		if(!$con){
-			die('Could not connect: ' . mysql_error());
-		}
-		mysql_select_db("CustomCupcakes", $con)
-		or die("Unable to select database: " . mysql_error());
+        //Retrieve the values from the session and the post. 
 		$user = $_SESSION['user'];
-		$filling = $_POST['fillingID'];
+        //If else statement in case there is no filling selected.
+		$filling = (!empty($_POST['fillingID']) ? "'".$_POST['fillingID']."'" : "NULL");
 		$frosting = $_POST['frostingID'];
 		$cake = $_POST['CakeID'];
-		$favName = $_POST['favoriteName'];
-		//Need to do toppings, depends on how GUI formats it.
+        //Fail-safe in case someone tries to hack us.
+		$favName = mysql_real_escape_string($_POST['favoriteName']);
+		$toppingsList = $_POST['toppingsID'];
+        //Gets the current id in the favorites table.
+        $favoriteStatus = mysql_fetch_assoc(mysql_query("SHOW TABLE STATUS LIKE 'Favorites'"));
+        $favoriteID = $favoriteStatus['Auto_increment'];
+        //Query to insert a favorite.
+		$query = "INSERT INTO Favorites(UserID, CakeID, FillingID, FrostingID, Name) VALUES ('$user', '$cake', $filling, '$frosting', '$favName')";
+		if(!mysql_query($query))
+		{
+			return false;
+		}
+        //Changes all values in toppingsList to (favoiteID, value) where value is a specific topping	
+        foreach ($toppingsList as &$value)
+        {   
+            $value = "('$favoriteID','$value')";
+        }
+        //Query to insert all toppings into FavoriteToppings 
+        //(with magical implode to concatenate all (favoriteID, value)'s from toppingsList with a comma.           
+        $query = "INSERT INTO FavoriteToppings(FavoriteID, ToppingsID) VALUES " . implode(",", $toppingsList);
+        if(!mysql_query($query))
+		{
+			return false;
+		}	        		
+        return true;
 	}
-
 }
 
 ?>
