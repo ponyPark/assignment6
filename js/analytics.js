6ad22db5aa8757282fd015ff90726d27e3eff20f
ekohn <ekohn@smu.edu>
2013-10-11 02:55:35 -0500

    Analytics page now works with sales data

--- js/analytics.js
+++ js/analytics.js
@@ -7,31 +7,24 @@ function init() {
 
 	var request = new XMLHttpRequest();
 	request.open("GET", url, true);
-    //request.send();
-    //request.onreadystatechange = function(e) {
+    request.send();
+    request.onreadystatechange = function(e) {
 
-       	//if(request.readyState === 4){
+       	if(request.readyState === 4){
             //save the response from server
-    //        var json = JSON.parse(request.responseText);
-    //        console.log(json);
-            var json = {"salesinfo":[
-            			{"cakesales":[{"Flavor":"Banana","Total":"20"},{"Flavor":"Dark Chocolate","Total":"10"},{"Flavor":"Dark Chocolate","Total":"1"},{"Flavor":"Dark Chocolate","Total":"1"},{"Flavor":"Dark Chocolate","Total":"1"}]},
-            			{"fillingsales":[{"Flavor":"Blackberry","Total":"1"},{"Flavor":"Strawbery","Total":"1"}]},
-            			{"frostingsales":[{"Flavor":"Buttered Popcorn","Total":"1"},{"Flavor":"Strawberry Cremem","Total":"1"}]},
-            			{"toppingsales":[{"Flavor":"Craisins","Total":"1"},{"Flavor":"M&M's","Total":"3"},{"Flavor":"Maraschino Cherries","Total":"6"},{"Flavor":"Maraschino Cherries","Total":"4"},{"Flavor":"Maraschino Cherries","Total":"7"},{"Flavor":"Maraschino Cherries","Total":"1"}]}]};
-            
-            	createPie('cakesales', json.salesinfo[0].cakesales);
-            	createPie("fillingsales", json.salesinfo[1].fillingsales);
-            	createPie("frostingsales", json.salesinfo[2].frostingsales);
-            	createBar("toppingsales", json.salesinfo[3].toppingsales);
-          
-        //}
-//}
+            var json = JSON.parse(request.responseText);
+
+            createPie('cakesales', json.salesinfo[0].cakesales);
+            createPie("fillingsales", json.salesinfo[1].fillingsales);
+            createPie("frostingsales", json.salesinfo[2].frostingsales);
+            createBar("toppingsales", json.salesinfo[3].toppingsales);  
+        }
+	}
 };
 
 function createPie(name, data) {
 	var pieData = new Array();
-	
+
 	for (var i = 0, len = data.length; i < len; i++)
 		pieData[i] = [data[i].Flavor, parseInt(data[i].Total, 10)];
 
