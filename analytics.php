0ff6bec95be1de5d8d58a1f34f3c407f9120f5f5
ekohn <ekohn@smu.edu>
2013-10-11 02:29:21 -0500

    Decided to use a better chart library
    Using test data, cleaned up code somewhat

--- analytics.php
+++ analytics.php
@@ -14,8 +14,8 @@
     	<script src="js/rgraph/RGraph.common.core.js"></script>
     	<script src="js/rgraph/RGraph.bar.js"></script>
     	<script src="js/rgraph/RGraph.pie.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js">
-</script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="http://code.highcharts.com/2.2.4/highcharts.js"></script>
 
 
 
@@ -25,5 +25,28 @@
 		<header>
 			<h1>Custom Cupcakes Analytics</h1>
 		</header>
+            
+            <table width='100%'>
+                <tr>
+                    <td width='33%'><h2>Cake Sales</h2></td>
+                    
+                    <td width='33%'><h2>Frosting Sales</h2></td>
+                    <td width='33%'><h2>Filling Sales</h2></td>
+                </tr>
+                <tr>
+                    <td><div id='cakesales'></div></td>
+                     <td>  <div id='frostingsales'></div></td>
+
+                    
+               <td> <div id='fillingsales'></div></td>
+           </tr>
+       </table>
+                    <h2>Topping Sales</h2>
+                            <canvas id='toppingsales' height=250></canvas>
+
+</td>
+</tr>
+
+        </table>
 	</body>
 </html>
--- js/analytics.js
+++ js/analytics.js
@@ -2,74 +2,68 @@
 October, 2013*/
 
 function init() {
-	//$("head").append('<script src="js/rgraph/RGraph.common.core.js"></script>');
-	//$("head").append('<script src="js/rgraph/RGraph.pie.js"></script>');
-	//$("head").append('<script src="js/rgraph/RGraph.bar.js"></script>');
 	var json;
 	var url = './SalesInformation.php';
 
 	var request = new XMLHttpRequest();
 	request.open("GET", url, true);
-    request.send();
-    request.onreadystatechange = function(e) {
+    //request.send();
+    //request.onreadystatechange = function(e) {
 
-       	if(request.readyState === 4){
+       	//if(request.readyState === 4){
             //save the response from server
-            var json = JSON.parse(request.responseText);
-            console.log(json);
-            /**var json = {"salesinfo":[
-            			{"cakesales":[{"Flavor":"Banana","Total":"1"},{"Flavor":"Dark Chocolate","Total":"1"}]},
+    //        var json = JSON.parse(request.responseText);
+    //        console.log(json);
+            var json = {"salesinfo":[
+            			{"cakesales":[{"Flavor":"Banana","Total":"20"},{"Flavor":"Dark Chocolate","Total":"10"},{"Flavor":"Dark Chocolate","Total":"1"},{"Flavor":"Dark Chocolate","Total":"1"},{"Flavor":"Dark Chocolate","Total":"1"}]},
             			{"fillingsales":[{"Flavor":"Blackberry","Total":"1"},{"Flavor":"Strawbery","Total":"1"}]},
             			{"frostingsales":[{"Flavor":"Buttered Popcorn","Total":"1"},{"Flavor":"Strawberry Cremem","Total":"1"}]},
-            			{"toppingsales":[{"Flavor":"Craisins","Total":"1"},{"Flavor":"M&M's","Total":"1"},{"Flavor":"Maraschino Cherries","Total":"1"}]}]};
-            */
-            	createPie("cakesales", json.salesinfo[0].cakesales);
+            			{"toppingsales":[{"Flavor":"Craisins","Total":"1"},{"Flavor":"M&M's","Total":"1"},{"Flavor":"Maraschino Cherries","Total":"1"},{"Flavor":"Maraschino Cherries","Total":"1"},{"Flavor":"Maraschino Cherries","Total":"1"},{"Flavor":"Maraschino Cherries","Total":"1"}]}]};
+            
+            	createPie('cakesales', json.salesinfo[0].cakesales);
             	createPie("fillingsales", json.salesinfo[1].fillingsales);
             	createPie("frostingsales", json.salesinfo[2].frostingsales);
             	createBar("toppingsales", json.salesinfo[3].toppingsales);
-
-
           
-        }
-}
-}
+        //}
+//}
+};
 
 function createPie(name, data) {
-	canvas = document.createElement("canvas");
-	console.log(data);
-	canvas.id = name;
-	canvas.width = "400";
-	canvas.height = "200";
-	canvas.display = "block";
-	document.body.appendChild(canvas);
+	var pieData = new Array();
+	for (var i = 0, len = data.length; i < len; i++)
+		pieData[i] = [data[i].Flavor, parseInt(data[i].Total, 10)];
+	console.log(pieData);
 
-	var flavors = new Array();
-	var totals = new Array();
+	var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: name,
+            backgroundColor: 'rgba(255, 255, 255, 0)',
+            plotBorderWidth: null,
+            plotShadow: false,
+            height: 300,
+            margin: [40, 40, 80, 40]
+        },
+        credits: {
+            enabled: false
+        },
+        title: {
+            text: null
+        },
+ 
+        series: [{
+            type: 'pie',
+            name: 'Total Orders',
+            data: pieData
+        }]    
+    });
 
-	for (var i = 0, len = data.length; i < len; i++) {
-		flavors[i] = data[i].Flavor;
-		totals[i] = parseInt(data[i].Total, 10);
-	}
-
-	console.log(flavors);
-	console.log(totals);
-
-	var pie = new RGraph.Pie(name, totals)
-		.Set('chart.labels', flavors)
-		.Set('chart.title', name)
-		.Set('chart.text.size', 8)
-		.Draw();
-}		
+};
 
 function createBar(name, data) {
-	canvas = document.createElement("canvas");
 	console.log(data);
-
-	canvas.id = name;
-	canvas.width = "400";
-	canvas.height = "200";
-	canvas.display = "block";
-	document.body.appendChild(canvas);
+	var pageWidth = $(window).width();
+	document.getElementById(name).width = pageWidth;
 
 	var flavors = new Array();
 	var totals = new Array();
@@ -84,9 +78,8 @@ function createBar(name, data) {
 
 	var bar = new RGraph.Bar(name, totals)
 		.Set('chart.labels', flavors)
-		.Set('chart.title', name)
 		.Set('chart.text.size', 8)
 		.Draw();
-}		
+};	
 
 window.onload = init;
